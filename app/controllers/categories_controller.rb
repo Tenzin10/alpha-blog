f14c9b6f2b63b346242731b02948e1724408efbc
Tenzin10 <tenzin.dorjee.1534@gmail.com>
2017-07-05 23:11:57 +0530

    Update all views with categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,11 +5,13 @@ class CategoriesController < ApplicationController
 	end
 
 	def show
-		
+		@category = Category.find(params[:id])
+		@category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
 	end
 
 	def index
 		@categories = Category.paginate(page: params[:page], per_page: 5)
+
 	end
 
 	def create
